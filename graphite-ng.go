be73221e722a2fedad0b6d9396b23c801184b664
Stephan <stephan@scheller.com>
2014-09-18 12:17:18 +0000

    first rev of metric find endpoint

--- graphite-ng.go
+++ graphite-ng.go
@@ -19,6 +19,7 @@ import (
 	"text/template"
 	"time"
 	"unicode/utf8"
+	"regexp"
 )
 
 var (
@@ -41,6 +42,11 @@ type Params struct {
 	From    int32
 	Until   int32
 	Targets []Target
+}
+
+type Series struct {
+	Id   string
+	Leaf bool
 }
 
 // FieldsFuncWithDelim is like strings.FieldsFunc except it also returns
@@ -237,6 +243,126 @@ func MetricsListHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "]")
 }
 
+// untested for very large key range
+func MetricsFindHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	query_list := r.Form["query"]
+	if len(query_list) > 0 {
+                query := query_list[len(query_list) - 1]
+                // the List method could possibly be improved when enumerating keys doing select queries on certain keyspaces
+                // getAllMetrics gets entire key space
+                // filterAllMetrics(metrics, query) prepare for json rendering, returning leaf, id tuples
+                // renderJsonForFind turns list of ids and leafs into graphite query response format
+                metrics, err := getAllMetrics()
+                if err != nil {
+                        fmt.Fprintf(w, err.Error())
+                        return
+                }
+                filteredSet := filterMetrics(metrics, query)
+                fmt.Fprintf(w, "[")
+                skip_first_comma := true
+                for _, m := range filteredSet {
+                        fmt.Printf("%v\n", m)
+                        if !skip_first_comma {
+                                fmt.Fprintf(w, ", ")
+                        } else {
+                                skip_first_comma = false
+                        }
+                        fmt.Fprintf(w, renderJsonFromMetric(m))
+                }
+                fmt.Fprintf(w, "]")
+	} else {
+                fmt.Printf("Missing required parameter 'query'\n")
+                fmt.Fprintf(w, "Missing required parameter 'query'")
+                return
+        }
+}
+
+func getAllMetrics() ([]string, error) {
+        metrics := make([]string, 0)
+
+	for _, store := range stores.List {
+		list, err := (*store).List()
+		if err != nil {
+			fmt.Printf(err.Error())
+                        return nil, err
+		} else {
+                        metrics = append(metrics, list...)
+		}
+	}
+        return metrics, nil
+}
+
+func filterMetrics(keys []string, query string) ([]Series) {
+        metrics := make([]Series, 0)
+        setCheck := map[string]bool {}
+
+        //stats*.* => /^stats[^\.]*\.[^\.]*(hasChild::.*)$/
+        //if this matches, it is a leaf if hasChild is empty and no leaf it it contains something
+        pattern := "^"
+        for _, element := range query {
+                 if element == '*' {
+                          pattern += "[^\\.]*"
+                 } else if element == '.' {
+                          pattern += "\\."
+                 } else {
+                          pattern += string(element)
+                 }
+        }
+        pattern += "(.*)$"
+// FIXME, query=te, yields wrong result...
+        fmt.Printf("regex: %s\n", pattern)
+        regex := regexp.MustCompile(pattern)
+        for _, k := range keys {
+          matches := regex.MatchString(k)
+          if matches {
+            mmmk := regex.FindStringSubmatchIndex(k)
+            magic_leaf_identifier := (mmmk[3] - mmmk[2]) == 0
+
+            // last part of id is always replaced with node name
+            curr_key := k[mmmk[0]:mmmk[2]]
+            dots := strings.Split(curr_key, ".")
+            last_part := dots[len(dots) - 1]
+
+            id := last_part
+            lastDotIndex := strings.LastIndex(query, ".")
+            if lastDotIndex >= 0 {
+                     id = query[:lastDotIndex] + "." + last_part
+            }
+            // every leaf gets added as id
+            if magic_leaf_identifier {
+                    metrics = append(metrics, Series{Id: id, Leaf: true})
+            // every non leaf gets added with the * if it doesn't exist in map, otherwise add it
+            } else {
+                    if setCheck[id] == false {
+                            metrics = append(metrics, Series{Id: id, Leaf: false})
+                            setCheck[id] = true
+                    }
+            }
+          }
+        }
+
+        return metrics
+}
+
+func renderJsonFromMetric(s Series) string {
+        str := "{\"leaf\": "
+        l := ""
+        c := ""
+        if s.Leaf == true {
+                l = "1"
+                c = "0"
+        } else {
+                l = "0"
+                c = "1"
+        }
+        text := strings.Split(s.Id, ".")
+        str += l + ", \"context\": {}, \"text\": \"" + text[len(text) - 1] + "\", \"expandable\": " + c
+        str += ", \"id\": \"" + s.Id + "\", \"allowChildren\": " + c + "}"
+        return str
+}
+// {"leaf": 0, "context": {}, "text": "cache", "expandable": 1, "id": "carbon.agents.staging-graphite-999-a.cache", "allowChildren": 1}
+// leaf is opposite of allowChildrem, text is last . split of id, * selects all at same depth, explicit key only returns single node
 func corsHeaders(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -271,6 +397,7 @@ func main() {
 	http.HandleFunc("/render", corsHeaders(renderHandler))
 	http.HandleFunc("/render/", corsHeaders(renderHandler))
 	http.HandleFunc("/metrics/index.json", corsHeaders(MetricsListHandler))
+	http.HandleFunc("/metrics/find/", corsHeaders(MetricsFindHandler))
 	fmt.Println("listening on", config.ListenAddr)
 	http.ListenAndServe(config.ListenAddr, nil)
 }
